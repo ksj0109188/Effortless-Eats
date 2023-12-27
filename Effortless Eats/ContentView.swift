3270ebcbf97e3a78a0d6a8f26958e4f4ba8fb50e
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2023-12-27 14:42:38 +0900

    Feat: added KaKaoMapsSDK

---
+++ Effortless Eats/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Effortless Eats
+//
+//  Created by 김성준 on 12/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button {
+                
+            } label: {
+                Text("랜덤 추천받기")
+            }
+
+        }
+        .padding()
+    }
+}
+
+func getFood() {
+    
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Effortless Eats/Effortless_EatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Effortless_EatsApp.swift
+//  Effortless Eats
+//
+//  Created by 김성준 on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Effortless_EatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
