12b03c9a9b544f32229ac03eea61fa9a480f00a6
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2024-01-01 15:48:56 +0900

    Refactor: split the mainView with contentView

--- Effortless Eats/ContentView.swift
+++ Effortless Eats/ContentView.swift
@@ -8,24 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    let kaka = RandomRecommendFoodStore()
-    
     var body: some View {
-        VStack {
-            Button {
-                getFood(kaka: kaka)
-            } label: {
-                Text("랜덤 추천받기")
-            }
-        }
-        .padding()
-        
+        HomeView()
     }
-}
-
-func getFood(kaka: RandomRecommendFoodStore) {
-    kaka.음식추천받기(radius: 20000)
-    
 }
 
 #Preview {
--- Effortless Eats/HomeView.swift
+++ Effortless Eats/HomeView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var isRecommendButtonCilcked: Bool = false
+    let kaka = RandomRecommendFoodStore()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button {
+                getFood(kaka: kaka)
+                isRecommendButtonCilcked.toggle()
+            } label: {
+                Text("랜덤 추천받기")
+            }
+        }
+        .alert("추첨", isPresented: $isRecommendButtonCilcked, presenting: Text("f"), actions: { T in
+            HStack{
+                Text("다시뽑기")
+                Text("확정")
+            }
+        })
+        
+        .padding()
+        
+    }
+    
+    func getFood(kaka: RandomRecommendFoodStore) {
+        kaka.음식추천받기(radius: 20000)
+        
     }
 }
 
